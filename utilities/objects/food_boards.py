ea220e8727f7cbfc0d164debb1995aae6cb66552
Chuck Meyer <chuck.meyer@algolia.com>
2021-11-03 15:28:57 -0400

    Improved weighting for food.

--- utilities/objects/food_boards.py
+++ utilities/objects/food_boards.py
@@ -1,95 +1,94 @@
-from typing import List, Dict, Set
+from typing import List, Set
 from copy import deepcopy
 import json
 
+
 def add_board(boards: Set[str], board: str) -> List[str]:
-  boards.add(board)
-  return boards
+    boards.add(board)
+    return boards
 
 
 def print_board(size: int, board: str):
-  print(board)    
-  print(len(board))
-  for i in range(len(board),0,-size):
-    row = ''
-    for j in range(i - size, i):
-      row += board[j]
+    print(board)
+    print(len(board))
+    for i in range(len(board), 0, -size):
+        row = ''
+        for j in range(i - size, i):
+            row += board[j]
     print(row)
 
 
 def inverse_board(board: str) -> str:
-  board = board.replace('S', 'X') 
-  board = board.replace('O', 'S') 
-  board = board.replace('X', 'O') 
-  return board
+    board = board.replace('S', 'X')
+    board = board.replace('O', 'S')
+    board = board.replace('X', 'O')
+    return board
 
 
 def in_boards(boards: List[dict], board: dict) -> bool:
-  return board in boards
+    return board in boards
 
 
 def create_board(board_size: int, apples: List[dict]) -> str:
     """
 
-    return: A matrix representing the blocks directly surrounding the snake's head.
+    return: A string representing food directly surrounding the snake's head.
     """
     board = ''
     head = board_size//2
     for y in range(0, board_size):
-      for x in range(0, board_size):
-        space = 'S'
-        if x == head and y == head:
-          space = "H"
-        else:
-          for apple in apples:
-            if x == apple["x"] and y == apple["y"]:
-              space = "O"
-        board += space
+        for x in range(0, board_size):
+            space = 'S'
+            if x == head and y == head:
+                space = "H"
+            else:
+                for apple in apples:
+                    if x == apple["x"] and y == apple["y"]:
+                        space = "O"
+            board += space
     return board
 
 
 def save_boards(boards: List[dict], filename: str):
-  # Write the records to a file
-  with open(filename, 'w') as outfile:
-    json.dump(boards, outfile, indent=2)
+    # Write the records to a file
+    with open(filename, 'w') as outfile:
+        json.dump(boards, outfile, indent=2)
 
 
 def add_apple(apples: List[dict], x: int, y: int) -> List[dict]:
-  #print("Adding apple at {} x {}".format(x, y))
-  apples.append({ "x": x, "y": y})
-  #print("Apple added: {}".format(apples))
-  return apples
+    apples.append({"x": x, "y": y})
+    return apples
 
 
-def in_apples(apples: List[dict], x: int, y:int) -> bool:
-  match = False
-  for apple in apples:
-    if x == apple["x"] and y == apple["y"]:
-      match = True
-  return match
+def in_apples(apples: List[dict], x: int, y: int) -> bool:
+    match = False
+    for apple in apples:
+        if x == apple["x"] and y == apple["y"]:
+            match = True
+    return match
 
 
 def recursive_boards(board_size: int = 3, depth: int = 0, apples: List[dict] = [], boards: Set[str] = set()):
-  head = board_size//2
-  max_depth = (board_size**2-1)
-  #print("  At depth {} with {}".format(depth, apples))
-  board = create_board(board_size, apples)
-  boards = add_board(boards, board)
-  # We can halve the the recursion depth by always adding the inverse of the generated board
-  boards = add_board(boards, inverse_board(board))
-  if depth != max_depth:
-    for y in range(0,board_size):
-      for x in range(0,board_size):
-        local_apples = deepcopy(apples)
-        if not (x == head and y == head) and not (in_apples(local_apples, x, y)):
-          local_apples = add_apple(local_apples, x, y)
-          local_board = create_board(board_size, local_apples)
-          if not in_boards(boards, local_board):
-            boards = recursive_boards(board_size, depth+1, local_apples, boards)
+    head = board_size//2
+    max_depth = (board_size**2-1)
+    # print("  At depth {} with {}".format(depth, apples))
+    board = create_board(board_size, apples)
+    boards = add_board(boards, board)
+    # We can halve the the recursion depth by also adding the inverse of the
+    # generated board
+    boards = add_board(boards, inverse_board(board))
+    if depth != max_depth:
+        for y in range(0, board_size):
+            for x in range(0, board_size):
+                local_apples = deepcopy(apples)
+                if not (x == head and y == head) and not (in_apples(local_apples, x, y)):
+                    local_apples = add_apple(local_apples, x, y)
+                    local_board = create_board(board_size, local_apples)
+                    if not in_boards(boards, local_board):
+                        boards = recursive_boards(board_size, depth+1, local_apples, boards)
     print("{} boards({}  {})".format(len(boards), board, inverse_board(board)))
-  return boards
+    return boards
 
 
 def generate_boards(board_size: int = 3) -> Set[str]:
-  return recursive_boards(board_size)
-  
+    return recursive_boards(board_size)
--- utilities/objects/food_plays.py
+++ utilities/objects/food_plays.py
@@ -2,43 +2,55 @@ from typing import List
 
 
 def find_best_moves(board_size: int, board: str) -> List[str]:
-  head = board_size//2
-  preferred = {
-    "left": 0,
-    "right": 0,
-    "up": 0,
-    "down": 0,
-  }
+    head = board_size//2
+    preferred = {
+        "left": 0,
+        "right": 0,
+        "up": 0,
+        "down": 0,
+    }
 
-  for loc, value in enumerate(board):
-    if value == 'O':
-      x_diff = loc % board_size
-      y_diff = loc // board_size
-      if x_diff < head:
-        preferred["left"] += 1 + x_diff
-      if x_diff > head:
-        preferred["right"] += board_size - x_diff
-      if y_diff < head:
-        preferred["down"] += 1 + y_diff
-      if y_diff > head:
-        preferred["up"] += board_size - y_diff
-  best_move_score = max(preferred, key=preferred.get)
-  best_moves = []
-  if preferred[best_move_score] != 0:
-    for move in preferred.keys():
-      if preferred[move] == preferred[best_move_score]:
-        best_moves.append(move)
-  print("Best moves for {} are {}: {}".format(board, best_moves, preferred))
-  return best_moves  
+    for loc, value in enumerate(board):
+        if value == 'O':
+            x_diff = loc % board_size
+            y_diff = loc // board_size
+            x_weight = head - abs(x_diff - head)
+            y_weight = head - abs(y_diff - head)
+
+            # left
+            if x_diff < head:
+                print("{} left weight is 1 + {} + {}".format(loc, x_weight, y_weight))
+                preferred["left"] += 1 + x_weight + y_weight
+            # right
+            if x_diff > head:
+                print("{} right weight is 1 + {} + {}".format(loc, x_weight, y_weight))
+                preferred["right"] += 1 + x_weight + y_weight
+            # down
+            if y_diff < head:
+                print("{} down weight is 1 + {} + {}".format(loc, x_weight, y_weight))
+                preferred["down"] += 1 + x_weight + y_weight
+            # up
+            if y_diff > head:
+                print("{} up weight is 1 + {} + {}".format(loc, x_weight, y_weight))
+                preferred["up"] += 1 + x_weight + y_weight
+
+    best_move_score = max(preferred, key=preferred.get)
+    best_moves = []
+    if preferred[best_move_score] != 0:
+        for move in preferred.keys():
+            if preferred[move] == preferred[best_move_score]:
+                best_moves.append(move)
+    print("Best moves for {} are {}: {}".format(board, best_moves, preferred))
+    return best_moves
 
 
 def create_play(board_size: int, board: str) -> dict:
     best_moves = find_best_moves(board_size, board)
     play = {
-      "objectID": board,
-      "board": board,
-      "best_move": best_moves,
-      "description": "apple {}".format(' '.join(best_moves)),
-      "remove_moves": []
+        "objectID": board,
+        "board": board,
+        "best_move": best_moves,
+        "description": "apple {}".format(' '.join(best_moves)),
+        "remove_moves": []
     }
     return play
